d91ff891b0068b7f5f0786ffedeb0688e4ccbf7f
Luciano Adamczuk <lucianoadamczuk@gmail.com>
2024-11-23 13:03:47 -0300

    feat: :bug: add translations for solutions page

--- src/i18n/i18next.d.ts
+++ src/i18n/i18next.d.ts
@@ -1,15 +1,27 @@
 // import the original type declarations
-import "i18next";
+import 'i18next';
 // import all namespaces (for the default language, only)
-import navbar from "../../public/locales/en/navbar.json";
-import home from "../../public/locales/en/home.json";
+import navbar from '../../public/locales/en/navbar.json';
+import home from '../../public/locales/en/home.json';
+import footer from '../../public/locales/en/footer.json';
+import hireMe from '../../public/locales/en/hireMe.json';
+import metadata from '../../public/locales/en/metadata.json';
+import notFound from '../../public/locales/en/notFound.json';
+import shared from '../../public/locales/en/shared.json';
+import solutions from '../../public/locales/en/solutions.json';
 
-declare module "i18next" {
-  // Extend CustomTypeOptions
-  interface CustomTypeOptions {
-    resources: {
-      navbar: typeof navbar;
-      home: typeof home;
-    };
-  }
+declare module 'i18next' {
+	// Extend CustomTypeOptions
+	interface CustomTypeOptions {
+		resources: {
+			navbar: typeof navbar;
+			home: typeof home;
+			footer: typeof footer;
+			hireMe: typeof hireMe;
+			metadata: typeof metadata;
+			notFound: typeof notFound;
+			shared: typeof shared;
+			solutions: typeof solutions;
+		};
+	}
 }
